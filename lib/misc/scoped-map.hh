e8e7701c0d89038006c7e5f93074d845ff4046d7
Nolab <thibault.boutet@epita.fr>
2022-03-15 14:10:35 +0100

    add scoped map

--- lib/misc/scoped-map.hh
+++ lib/misc/scoped-map.hh
@@ -20,6 +20,29 @@ namespace misc
   template <typename Key, typename Data> class scoped_map
   {
     // FIXME: Some code was deleted here.
+  public:
+    // Associate value to key in the current scope
+    void put(const Key& key, const Data& value);
+
+    // If key was associated to some Data in the open scoped, 
+    // return the most recent insertion. 
+    // Otherwise, if Data is a pointer type, then return the empty pointer, 
+    // else throw a std::range_error. To implement this feature, see <type_traits>.
+    Data get(const Key &key) const;
+
+    // Send the content of this table on ostr in a human-readable manner,
+    // and return the stream.
+    std::ostream& dump(std::ostream& ostr) const;
+
+    // Open a new scope.
+    void scope_begin();
+
+    // Close the last scope, forgetting everything since the latest scope_begin()
+    void scope_end();
+
+  private:
+    // Top of the stack is the last element
+    std::vector<std::map<Key, Data>> stack_;
   };
 
   template <typename Key, typename Data>
--- lib/misc/scoped-map.hxx
+++ lib/misc/scoped-map.hxx
@@ -16,10 +16,54 @@
 namespace misc
 {
   // FIXME: Some code was deleted here.
+  template <typename Key, typename Data>
+  inline void scoped_map<Key, Data>::put(const Key& key, const Data& value)
+  {
+    stack_.rbegin()->insert(std::pair<Key, Data>(key, value));
+  }
 
   template <typename Key, typename Data>
-  inline std::ostream& operator<<(std::ostream& ostr,
-                                  const scoped_map<Key, Data>& tbl)
+  inline Data scoped_map<Key, Data>::get(const Key& key) const
+  {
+    for (auto i = stack_.rbegin(); i != stack_.rend(); i++)
+    {
+      auto find = i->find(key);
+      if (find != i->end())
+        return find->second;
+    }
+    if constexpr (std::is_pointer_v<Key>)
+      return nullptr;
+    throw std::range_error("Data not found");
+  }
+
+  template <typename Key, typename Data>
+  inline std::ostream& scoped_map<Key, Data>::dump(std::ostream& ostr) const
+  {
+    for (auto i = stack_.rbegin(); i != stack_.rend(); i++)
+    {
+      for (auto j = i->begin(); j != i->end(); i++)
+      {
+        ostr << '(' << j->first << ',' << j->second << ") ";
+      }
+    }
+    return ostr;
+  }
+
+  template <typename Key, typename Data> 
+  inline void scoped_map<Key, Data>::scope_begin()
+  {
+    stack_.push_back(std::map<Key,Data>());
+  }
+
+  template <typename Key, typename Data>
+  inline void scoped_map<Key, Data>::scope_end()
+  {
+    stack_.erase(stack_.end() - 1);
+  }
+
+    template <typename Key, typename Data>
+    inline std::ostream& operator<<(std::ostream& ostr,
+                                    const scoped_map<Key, Data>& tbl)
   {
     return tbl.dump(ostr);
   }
