0c0d7abb539603b4b1a551226283966bb263e827
jean.fechter <jean.fechter@epita.fr>
2022-03-15 19:30:17 +0100

    bind

--- src/bind/libbind.cc
+++ src/bind/libbind.cc
@@ -3,4 +3,16 @@
  ** \brief Define exported bind functions.
  */
 
-// FIXME: Some code was deleted here.
+// DONE: Some code was deleted here.
+
+#include <ast/chunk-list.hh>
+#include <ast/libast.hh>
+#include <bind/libbind.hh>
+#include <parse/libparse.hh>
+#include "binder.hh"
+
+namespace bind {
+    misc::error bind(ast::ChunkList c)
+    {
+    }
+}
--- src/bind/libbind.hh
+++ src/bind/libbind.hh
@@ -3,4 +3,15 @@
  ** \brief Interface of the bind module.
  */
 
-// FIXME: Some code was deleted here.
+// DONE: Some code was deleted here.
+
+#pragma once
+
+#include <ast/chunk-list.hh>
+#include <ast/libast.hh>
+#include <bind/libbind.hh>
+#include <parse/libparse.hh>
+
+namespace bind {
+    misc::error bind(ast::ChunkList c);
+}
