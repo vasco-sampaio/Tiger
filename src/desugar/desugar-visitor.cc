bddca4f126233f1868bd084a4c28e1c65e17de57
Nolab <thibault.boutet@epita.fr>
2022-04-06 14:28:19 +0200

    add for desugar

--- src/desugar/desugar-visitor.cc
+++ src/desugar/desugar-visitor.cc
@@ -11,6 +11,8 @@
 #include <parse/libparse.hh>
 #include <parse/tweast.hh>
 
+using namespace ast;
+
 namespace desugar
 {
   DesugarVisitor::DesugarVisitor(bool desugar_for_p, bool desugar_string_cmp_p)
@@ -85,7 +87,41 @@ namespace desugar
 
   void DesugarVisitor::operator()(const ast::ForExp& e)
   {
-    // FIXME: Some code was deleted here.
+    // DONE: Some code was deleted here.
+    const Location& location = e.location_get();
+    VarDec* vardec = recurse(e.vardec_get());
+    Exp* hi = recurse(e.hi_get());
+    Exp* body = recurse(e.body_get());
+
+    VarDec* lo = new VarDec(location, "_lo", vardec->type_name_get(), vardec->init_get());
+    VarDec* new_hi = new VarDec(location, "_hi", vardec->type_name_get(), hi);
+    VarDec* i_ = new VarDec(location, "i", vardec->type_name_get(), lo->init_get());
+
+    SimpleVar* simple_i = new SimpleVar(location, "i");
+    simple_i->def_set(i_);
+
+    SimpleVar* simple_hi = new SimpleVar(location, "_hi");
+    simple_i->def_set(new_hi);
+    OpExp* test = new OpExp(location, simple_i, ast::OpExp::Oper::le, simple_hi);
+
+    BreakExp* break_exp = new BreakExp(location);
+    AssignExp* assign = new AssignExp(location, simple_i, 
+      new ast::OpExp(location, simple_i, ast::OpExp::Oper::add, new IntExp(location, 1)));
+    
+    OpExp* comp = new ast::OpExp(location, simple_i, ast::OpExp::Oper::eq, simple_hi);
+    IfExp* ifexp = new IfExp(location, comp, break_exp, nullptr);
+
+    auto tmp = std::vector<ast::Exp*>({body, ifexp, assign});
+    SeqExp* seq = new SeqExp(location, &tmp);
+    WhileExp* whileExp = new WhileExp(location, new IntExp(location, 1), seq);
+
+    IfExp* finalIf = new IfExp(location, test, whileExp, nullptr);
+
+    ChunkList* decs = new ChunkList(location);
+    decs->emplace_back(lo);
+    decs->emplace_back(new_hi);
+    decs->emplace_back(i_);
+    result_ = new LetExp(location, decs, finalIf);
   }
 
 } // namespace desugar
