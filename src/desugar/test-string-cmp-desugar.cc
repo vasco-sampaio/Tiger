609a83dc8a4c6deebe2a2ad59d657084ccd48cbe
Nolab <thibault.boutet@epita.fr>
2022-04-06 11:45:03 +0200

    recover test files

--- src/desugar/test-string-cmp-desugar.cc
+++ src/desugar/test-string-cmp-desugar.cc
@@ -20,7 +20,7 @@ const char* program_name = "test-string-cmp-desugar";
 
 static void test_string_desugaring(const std::string& oper)
 {
-  /*Exp* tree =
+  Exp* tree =
     parse::parse("let primitive streq(s1 : string, s2 : string) : int\n"
                  "   primitive strcmp(s1 : string, s2 : string) : int\n"
                  "in\n"
@@ -29,22 +29,22 @@ static void test_string_desugaring(const std::string& oper)
                  + " \"bar\")\n"
                    "end\n");
   type::TypeChecker type;
-  type(tree);*/
+  type(tree);
 
-  //std::cout << "/* === Original tree...  */\n" << *tree << '\n';
+  std::cout << "/* === Original tree...  */\n" << *tree << '\n';
 
-  //DesugarVisitor desugar(false, true);
-  //tree->accept(desugar);
-  //delete tree;
-  //tree = nullptr;
-  //std::cout << "/* === Desugared tree...  */\n"
-   //         << *desugar.result_get() << '\n';
-  //delete desugar.result_get();
+  DesugarVisitor desugar(false, true);
+  tree->accept(desugar);
+  delete tree;
+  tree = nullptr;
+  std::cout << "/* === Desugared tree...  */\n"
+            << *desugar.result_get() << '\n';
+  delete desugar.result_get();
 }
 
 int main()
 {
-  /*// Desugaring `"foo" = "bar"' as `streq("foo", "bar")'.
+  // Desugaring `"foo" = "bar"' as `streq("foo", "bar")'.
   std::cout << "First test...\n";
   test_string_desugaring("=");
   std::cout << std::endl;
@@ -56,5 +56,5 @@ int main()
 
   // Desugaring `"foo" >= "bar"' as `strcmp("foo", "bar") >= 0'.
   std::cout << "Third test...\n";
-  test_string_desugaring(">=");*/
+  test_string_desugaring(">=");
 }
